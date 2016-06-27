99eded716442df0951f7fe71ae09d20e47884ebb
David Lai <dayvedde@gmail.com>
2016-06-27 19:39:16 -0400

    Get get to somewhat work

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,6 +3,33 @@
 /* Controller */
 angular.module('myApp.controllers', []).
 	controller('AppCtrl', function ($scope, $http) {
+		$scope.styleCode = "";
+		$scope.colorCode = "";
+		$scope.sizeCode  = "";
+
 		// Insert controller code here
-		
+    	$scope.test = function() {
+			$http.get('/api/apparel', {
+				params : {	styleCode : $scope.styleCode, 
+							colorCode : $scope.colorCode, 
+							sizeCode: $scope.sizeCode
+						}
+			})
+			.success(function(response){
+				console.log('Success');
+				console.log(response);
+			})
+			.error(function(err){
+	 			console.log(err);
+			});
+		}
 	});
+
+// $("#btn-submit").click(function(){
+// 	var styleCode = $("#input-style-code").val();
+// 	var colorCode = $("#input-color-code").val();
+// 	var sizeCode = $("#input-size-code").val();
+
+// 	getApparelPrice(styleCode, colorCode, sizeCode);
+// 	console.log("Style, color, size", styleCode, colorCode, sizeCode);
+// })
--- routes/api.js
+++ routes/api.js
@@ -7,9 +7,12 @@ var q           = require('q');
 var api         = express.Router();
 var db          = require('../config/db').connection;
 
+
 // API endpoint for /api/apparel
 api.get('/api/apparel/:styleCode?', function(req, res) {
 	// Insert Apparel API code here
+	console.log("getting");
+	console.log(res.req.query);
 
 });
 
@@ -23,6 +26,7 @@ api.post('/api/quote', function(req, res) {
 var getApparelPrice = function getPrice(style_code, color_code, size_code) {
 	var	apparelPriceDeferred = q.defer();
 
+	alert("Hello in get apparel");
 	// Format the Inventory API endpoint as explained in the documentation
 	https.get('// INSERT INVENTORY API END POINT', function(res) {
 		res.on('data', function (data) {
@@ -36,5 +40,16 @@ var getApparelPrice = function getPrice(style_code, color_code, size_code) {
 
 	return apparelPriceDeferred.promise;
 }
+
+function t1() {
+	alert("t1");
+}
+
+
+// db.query('SELECT * FROM apparel', function(err, rows, fields) {
+// 	if (err) throw err;
+
+// 	console.log('The solution is: ', rows);
+// });
 
 module.exports = api;
